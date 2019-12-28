a9421b04e311d0ba6085c22275edbc8590ad5f7a
Kleiton Bezerra <kleiton2102@gmail.com>
2019-12-28 11:20:04 -0300

    Criação do cors

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,10 +3,19 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const morgan = require('morgan')
+const allowCors = require('./cors')
+
+morgan('combined', {
+  skip: function (req, res) { return res.statusCode < 400 }
+})
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function() {
   console.log(`BACKEND is running on port ${port}.`)
 })
+
+module.exports = server
